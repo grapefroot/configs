d58785cde6f64c83a0b12b44a5ea6e9814c28754
Dmitriy Salnikov <salnikov.dmitri@gmail.com>
2016-11-27 01:38:28 +0300

    update

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,16 +6,20 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Hooks.SetWMName
 import Graphics.X11.ExtraTypes.XF86
+import XMonad.Hooks.ManageHelpers
  
 myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
+    [  isFullscreen             --> doFullFloat
+     , className =? "Vncviewer" --> doFloat
+     , className =? "Gimp"      --> doFloat
     ]
  
 main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar  /home/grapefroot/.xmobarrc"
+    xmproc <- spawnPipe "xmobar  /home/grapefroot/.xmobarrc"
     xmonad $ defaultConfig
-        { startupHook = setWMName "LG3D"
+        { startupHook =  
+        	 	do setWMName "LG3D"
+		 	   spawn "conky --daemonize -b"
 	, manageHook = manageDocks <+> myManageHook -- make sure to include myManageHook definition from above
                         <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
@@ -27,10 +31,16 @@ main = do
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         ,((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        ,((0, xK_Print), spawn "scrot")
-	,((0, xF86XK_KbdBrightnessUp), spawn "xbacklight +10")
-	,((0, xF86XK_KbdBrightnessDown), spawn "xbacklight -10")
+	,((0, xF86XK_MonBrightnessUp), spawn "xbacklight +17")
+	,((0, xF86XK_MonBrightnessDown), spawn "xbacklight -17")
 	,((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2-")
 	,((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2+")
-	,((0, xF86XK_AudioMute), spawn "amixer -D pulse set Master toggle")]
+	,((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
+	,((0, xF86XK_AudioPlay), spawn "clementine --play-pause")
+	,((0, xF86XK_AudioPrev), spawn "clementine --previous")
+	,((0, xF86XK_AudioNext), spawn "clementine --next")
+
+
+	,((mod4Mask, xK_s), spawn "dmenu_extended_run \" -> Internet search: \" \"Google\"")]
+	
 
