b4302468fd10b2cc617031749aa395e3cf705712
grapefroot <salnikov.dmitri@gmail.com>
2026-08-03 02:32:49 +0400

    lid switch

--- hosts/workstation/default.nix
+++ hosts/workstation/default.nix
@@ -100,6 +100,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  services.logind.settings.Login.HandleLidSwitch = "suspend";
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
 #  users.users."grapefroot" = {
 #    isNormalUser = true;
@@ -122,8 +124,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim
     kitty
     brightnessctl
   ];
